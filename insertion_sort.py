65920b86188da29e0f8b0d808ea6b43a59ab76a0
pyjka <alexey.neofitov@gmail.com>
2017-11-02 12:22:15 +0300

    insertion sort

--- insertion_sort.py
+++ insertion_sort.py
@@ -1,11 +1,11 @@
-def insertion(listik):
-	for i in range(1,len(listik)):
-		key = listik[i]
+def insertion(arr):
+	for i in range(1, len(arr)):
+		key = arr[i]
 		j = i-1
-		while j >=0 and listik[j] > key:
-			listik[j+1]=listik[j]
+		while j >=0 and arr[j] > key:
+			arr[j + 1]=arr[j]
 			j -=1
-		listik[j+1] = key
+		arr[j + 1] = key
 
 listik = [9,6,8,7,1,3,2,4,5,0]
 insertion(listik)
