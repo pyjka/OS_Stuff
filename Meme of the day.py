cf87143879e7ff4598d6c6d76919739aaa68d275
pyjka <alexey.neofitov@gmail.com>
2017-11-02 13:28:56 +0300

    meme of the day

--- Meme of the day.py
+++ Meme of the day.py
@@ -5,18 +5,30 @@ import pyninegag
 """ Sends random meme from 9gag via iMessage"""
 
 
-"""send_message()
+def send_message():
+    def ninegag():
+        url = 'http://9gag.com/hot'
+        for i in pyninegag.get_articles(url, max_pages=15):
+            return i['url']
+
+    cmd = """osascript<<END
+
+    tell application "Messages"
+
+    send "MEME OF THE DAY is %s " to buddy "+79163035861" of (service 1 whose service type is iMessage) 
+
+    end tell
+
+    END""" % ninegag()
+    os.system(cmd)
+
+send_message()
 
 schedule.every().day.at("23:00").do(send_message)
 while True:
     schedule.run_pending()
-    time.sleep(1)"""
+    time.sleep(1)
 
 
-def ninegag():
-    url = 'http://9gag.com/hot'
-    for i in pyninegag.get_articles(url,max_pages=3):
-        return i
 
 
-ninegag()
